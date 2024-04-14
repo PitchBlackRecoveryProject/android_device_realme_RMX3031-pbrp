7d6a477d7bc8781183cd40af6da8ac812f035228
SathamHussainM <sathamhussain.m11@gmail.com>
2024-04-14 20:39:21 +0530

    RMX3031: Build missing ```libresetprop.so``` from source
    F linker  : CANNOT LINK EXECUTABLE "/system/bin/recovery": library "libresetprop.so" not found: needed by main executable
    
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- device.mk
+++ device.mk
@@ -21,3 +21,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libresetprop
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libresetprop.so
